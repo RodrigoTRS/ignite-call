3dc977a150ecd5ea3deed9882b23df6dc5c5b9fa
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-01-16 17:36:35 -0300

    Adding connect to calendar page

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { setCookie } from 'nookies'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
@@ -11,11 +12,29 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      name,
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'User already exists',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignite-call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    path: '/',
   })
 
   return res.status(201).json(user)
