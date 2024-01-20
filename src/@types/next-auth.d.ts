f94e57b885bebe9d20bc445991e6f3cd3df86e94
Rodrigo Eldorado <rodrigoteix1998@gmail.com>
2024-01-19 23:40:26 -0300

    Finishing adapter

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth"
+import NextAuth from 'next-auth'
 
 declare module 'next-auth' {
-    interface User {
-        id: string
-        name: string
-        email: string
-        username: string
-        avatar_url: string
-    }
+  interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
 }
